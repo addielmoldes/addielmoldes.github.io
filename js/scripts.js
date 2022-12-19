a6ee2e10a2e034a4dfe0c7eb56e6866ba61cad0c
addielmoldes <addielpunk99@gmail.com>
2022-12-19 05:13:59 -0500

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+let home = document.querySelector('#home');
+let about = document.querySelector('#about');
+let contact = document.querySelector('#contact');
+let homeBtn = document.querySelector('#homeBtn');
+let aboutBtn = document.querySelector('#aboutBtn');
+let contactBtn = document.querySelector('#contactBtn');
+
+homeBtn.addEventListener('click', function () {
+    about.style.display = 'none';
+    aboutBtn.className = 'navitem';
+    contact.style.display = 'none';
+    contactBtn.className = 'navitem';
+    home.style.display = 'flex';
+    this.className = 'navitem-active';
+});
+
+aboutBtn.addEventListener('click', function () {
+    home.style.display = 'none';
+    homeBtn.className = 'navitem';
+    contact.style.display = 'none';
+    contactBtn.className = 'navitem';
+    about.style.display = 'flex';
+    this.className = 'navitem-active';
+});
+
+contactBtn.addEventListener('click', function () {
+    home.style.display = 'none';
+    homeBtn.className = 'navitem';
+    about.style.display = 'none';
+    aboutBtn.className = 'navitem';
+    contact.style.display = 'flex';
+    this.className = 'navitem-active';
+});
