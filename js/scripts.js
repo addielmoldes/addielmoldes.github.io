4e37231ac66e4b01bf6594802eba35bd719916ba
addielmoldes <addielpunk99@gmail.com>
2022-12-20 03:28:23 -0500

    Add about button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let home = document.querySelector('#home');
 let about = document.querySelector('#about');
 let contact = document.querySelector('#contact');
+let goBtn = document.querySelector('#goBtn');
 let homeBtn = document.querySelector('#homeBtn');
 let aboutBtn = document.querySelector('#aboutBtn');
 let contactBtn = document.querySelector('#contactBtn');
@@ -12,6 +13,15 @@ homeBtn.addEventListener('click', function () {
     contactBtn.className = 'navitem';
     home.style.display = 'flex';
     this.className = 'navitem-active';
+});
+
+goBtn.addEventListener('click', function(){
+    home.style.display = 'none';
+    homeBtn.className = 'navitem';
+    contact.style.display = 'none';
+    contactBtn.className = 'navitem';
+    about.style.display = 'flex';
+    aboutBtn.className = 'navitem-active';
 });
 
 aboutBtn.addEventListener('click', function () {
