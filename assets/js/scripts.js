b9241fc62df2ad899aa3ddf0f58ae86863b5749e
addielmoldes <addielpunk99@gmail.com>
2022-12-26 16:50:50 -0500

    BIG CHANGES CONTINUE

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,53 +1,6 @@
-let home = document.querySelector('#home');
-let about = document.querySelector('#about');
-let contact = document.querySelector('#contact');
-let homeBtn = document.querySelector('#homeBtn');
-let aboutBtn = document.querySelector('#aboutBtn');
-let contactBtn = document.querySelector('#contactBtn');
 let message = document.querySelector('#message');
 let form = document.querySelector('#form');
 let email = document.querySelector('#email');
-
-function homeInactive(){
-    home.style.display = 'none';
-    homeBtn.className = 'navitem';
-}
-
-function aboutInactive(){
-    about.style.display = 'none';
-    aboutBtn.className = 'navitem';
-}
-
-function contactInactive(){
-    contact.style.display = 'none';
-    contactBtn.className = 'navitem';
-}
-
-// homeBtn.addEventListener('click', function () {
-//     aboutInactive();
-//     contactInactive();
-//     home.style.display = 'flex';
-//     this.className = 'navitem-active';
-// });
-
-// aboutBtn.addEventListener('click', function () {
-//     homeInactive();
-//     contactInactive();
-//     about.style.display = 'flex';
-//     this.className = 'navitem-active';
-// });
-
-// contactBtn.addEventListener('click', function () {
-//     homeInactive();
-//     aboutInactive();
-//     contact.style.display = 'flex';
-//     this.className = 'navitem-active';
-// });
-
-// message.addEventListener('input', function () {
-//     this.style.height = 'auto';
-//     this.style.height = this.scrollHeight + 'px';
-// });
 
 form.addEventListener('submit', function (e) {
     if (email.value == '' || message.value == '') {
